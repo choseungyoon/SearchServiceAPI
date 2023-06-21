e5ab322d7041d8884c54be1d35f2c7cd8f21983f
choseungyoon <cho31250@gmail.com>
2023-06-21 20:55:37 +0900

    Try call Kakao, Naver API 3 times when error

--- src/main/java/com/service/search/service/api/KakaoSearchApi.java
+++ src/main/java/com/service/search/service/api/KakaoSearchApi.java
@@ -17,29 +17,46 @@ import java.util.List;
 public class KakaoSearchApi implements SearchApi{
 
     private final RestTemplate restTemplate;
+    private final int MAX_RETRIES = 3;
 
     @Override
     public List<Place> searchPlace(String query , int size) {
+        List<Place> places = new ArrayList<>();
+        int retries = 0;
         String url = "https://dapi.kakao.com/v2/local/search/keyword.json";
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "KakaoAK " +  "a063f349c96bcea82f626f2f9ae698de");
 
-        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<KakaoApiResponse> responseEntity = restTemplate.exchange(
-                url + "?query=" + query + "&size=" + size, HttpMethod.GET, requestEntity, KakaoApiResponse.class);
+        while (retries < MAX_RETRIES) {
+            try {
+                HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+                ResponseEntity<KakaoApiResponse> responseEntity = restTemplate.exchange(
+                        url + "?query=" + query + "&size=" + size, HttpMethod.GET, requestEntity, KakaoApiResponse.class);
 
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                    KakaoApiResponse kakaoApiResponse = responseEntity.getBody();
+                    kakaoApiResponse.getDocuments().forEach(document -> {
+                        places.add(new Place(document.getPlace_name(), document.getRoad_address_name()));
+                    });
+                    break;
+                } else {
+                    retries++;
+                    if (retries >= MAX_RETRIES) {
+                        throw new RuntimeException("Fail call kakao API");
+                    }
+                }
+            }
+            catch (Exception e){
+                retries++;
+                if (retries >= MAX_RETRIES) {
+                    throw e; // Throw the exception if maximum retries reached
+                }
+            }
 
-            KakaoApiResponse kakaoApiResponse = responseEntity.getBody();
-            List<Place> places = new ArrayList<>();
-            kakaoApiResponse.getDocuments().forEach(document -> {
-                places.add(new Place(document.getPlace_name(), document.getRoad_address_name()));
-            });
-            return places;
-        } else {
-            throw new RuntimeException("API 호출에 실패하였습니다.");
         }
+
+        return places;
     }
 
     @Override
--- src/main/java/com/service/search/service/api/NaverSearchApi.java
+++ src/main/java/com/service/search/service/api/NaverSearchApi.java
@@ -16,30 +16,47 @@ import java.util.List;
 public class NaverSearchApi  implements SearchApi {
 
     private final RestTemplate restTemplate;
+    private final int MAX_RETRIES = 3;
 
     @Override
     public List<Place> searchPlace(String query, int size) {
+        List<Place> places = new ArrayList<>();
+        int retries = 0;
         String url = "https://openapi.naver.com/v1/search/local.json";
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Naver-Client-Id",  "QImLMIExKX0_uMERzAXF");
         headers.set("X-Naver-Client-Secret",  "73kabI9dw4");
 
-        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<NaverApiResponse> responseEntity = restTemplate.exchange(
-                url + "?query=" + query + "&display=" + size, HttpMethod.GET, requestEntity, NaverApiResponse.class);
+        while (retries < MAX_RETRIES) {
+            try{
+                HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+                ResponseEntity<NaverApiResponse> responseEntity = restTemplate.exchange(
+                        url + "?query=" + query + "&display=" + size, HttpMethod.GET, requestEntity, NaverApiResponse.class);
 
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                if (responseEntity.getStatusCode() == HttpStatus.OK) {
 
-            NaverApiResponse kakaoApiResponse = responseEntity.getBody();
-            List<Place> places = new ArrayList<>();
-            kakaoApiResponse.getItems().forEach(document -> {
-                places.add(new Place(document.getTitle().replaceAll("<[^>]+>", ""), document.getRoadAddress()));
-            });
-            return places;
-        } else {
-            throw new RuntimeException("API 호출에 실패하였습니다.");
+                    NaverApiResponse kakaoApiResponse = responseEntity.getBody();
+
+                    kakaoApiResponse.getItems().forEach(document -> {
+                        places.add(new Place(document.getTitle().replaceAll("<[^>]+>", ""), document.getRoadAddress()));
+                    });
+                    break;
+
+                } else {
+                    retries++;
+                    if (retries >= MAX_RETRIES) {
+                        throw new RuntimeException("Fail call naver API");
+                    }
+                }
+            }catch (Exception e){
+                retries++;
+                if (retries >= MAX_RETRIES) {
+                    throw e; // Throw the exception if maximum retries reached
+                }
+            }
         }
+        return places;
     }
 
     @Override
